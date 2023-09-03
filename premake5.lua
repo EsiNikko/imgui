225cd9d263259147600d5983c60faa14f231eb88
Isaac Nikouei <nikouei.ehsan@gmail.com>
2023-09-03 12:14:50 +0330

    Cleanup the premake file.

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	cppdialect "C++17"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,13 +24,7 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-
+		
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
@@ -41,4 +36,3 @@ project "ImGui"
     filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
-        symbols "off"
